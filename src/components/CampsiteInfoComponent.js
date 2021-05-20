44cd94f4e47a6c73fa9599b265523fa52ae8a00f
Takyia <takyiahall@yahoo.com>
2021-05-20 19:55:16 -0400

    Inital upload

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+    function RenderCampsite({campsite}) {
+        return (
+            <div className="col-md-5 m-1">
+                        
+                        <Card>
+                        <CardImg width= "100%" src= {campsite.image} alt= {campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle> {campsite.name} </CardTitle>
+                            <CardTitle> {campsite.description} </CardTitle>
+                        </CardImgOverlay>
+                </Card>
+            </div>
+        )
+    }
+
+    function RenderComments({comments}) { 
+        if(comments) {
+            return (
+                <div className="col-5-md m-1">
+                    <h4> Comments </h4>
+                    {
+                        comments.map( 
+                            
+                                    comment => {
+                                            return (
+                                                <div key= {comment.id}>
+                                                    <p>
+                                                        {comment.text} <br />
+                                                        {comment.author}, 
+                                                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+
+                                                    </p>
+                                                </div>
+                                            );
+                                    }
+                            )
+                    }
+                </div>
+            );
+        }
+        return <div />;
+    }
+
+    function CampsiteInfo(props) {
+        if(props.campsite) {
+            return (
+                <div className="container">
+                    <div className="row"> 
+                        <RenderCampsite campsite = {props.campsite} />
+                        <RenderComments comments = {props.campsite.comments} />
+                    </div>
+                </div>
+            );
+        } 
+        return <div />;
+
+    }
+
+    
+
+
+
+
+export default CampsiteInfo;
